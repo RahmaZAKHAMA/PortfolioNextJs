e0ef55ea97bc520a8056157ba9645869d49314cb
RahmaZAKHAMA <rahmazakhama@gmail.com>
2024-09-16 10:16:46 +0100

    third commit

--- app/Projects/page.jsx
+++ app/Projects/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div className="h-72 bg-gray-400">
+    <div className="h-72 bg-gray-300">
       <NestedTable />
     </div>
   );
